8a0a196e387b8ee748c5a7283345dc2e352e3a5a
devstar1992 <75045062+devstar1992@users.noreply.github.com>
2021-10-19 00:46:04 +0800

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -51,7 +51,7 @@ class App extends Component {
         lastMintTime: localStorage.getItem(this.state.accountAddress),
       });
       this.state.lastMintTime === undefined || this.state.lastMintTime === null
-        ? (mintBtn.innerHTML = "Mint My Crypto Boy")
+        ? (mintBtn.innerHTML = "Mint")
         : this.checkIfCanMint(parseInt(this.state.lastMintTime));
     }
   };
@@ -65,7 +65,7 @@ class App extends Component {
       const diff = countDownTime - now;
       if (diff < 0) {
         mintBtn.removeAttribute("disabled");
-        mintBtn.innerHTML = "Mint My Crypto Boy";
+        mintBtn.innerHTML = "Mint";
         localStorage.removeItem(this.state.accountAddress);
         clearInterval(interval);
       } else {
